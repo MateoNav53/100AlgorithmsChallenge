c989905e633417abb258dba251a5d01b7df0ea76
Mateo Navarrette <mateonavarrette@Mateo-Navarrettes-MacBook-Pro-2.local>
2021-10-07 19:09:52 -0700

    add solutions

--- add/add.ts
+++ add/add.ts
@@ -1,9 +1,18 @@
 export function add(param1: number, param2: number): number {
-
+    return param1 + param2
 }
 
 export function add2(...param1: number[]): number {
+    
+    //solution with reduce
+    return param1.reduce((a, b) => a + b)
 
+    //for loop solution
+    // let total = 0
+    // for(let i = 0;i < param1.length;i++){
+    //     total += param1[i]
+    // }
+    // return total
 }
 // console.log(add(1, 2));
 // console.log(add(3, 2));
