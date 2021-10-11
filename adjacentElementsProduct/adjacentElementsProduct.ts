076bf76dc8e71aea81da158fe30ae47a26156a6d
Mateo Navarrette <mateonav24@gmail.com>
2021-10-10 18:31:28 -0700

    adjacent elements solution

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,6 +1,12 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-    //testing git commit
-    return 5
+    //Solution 1
+    let highest = 0
+    for(let i=0;i < inputArray.length;i++){
+        if(inputArray[i] * inputArray[i + 1] > highest){
+            highest = inputArray[i] * inputArray[i + 1]
+        }
+    }
+    return highest
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
