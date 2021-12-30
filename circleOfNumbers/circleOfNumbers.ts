a1a1e3a3e73d1a634bffeb0e1ba63963b3e52a45
Mateo Navarrette <mateonav24@gmail.com>
2021-12-29 21:09:56 -0800

    two solutions

--- circleOfNumbers/circleOfNumbers.ts
+++ circleOfNumbers/circleOfNumbers.ts
@@ -1,5 +1,9 @@
-export function circleOfNumbers(n: number, firstNumber: number): number {
-
+export function circleOfNumbers(n: number, firstNumber: number): any {
+    const half = Math.floor(n / 2)
+    if(firstNumber >= half){
+        return firstNumber - half
+    }
+    return firstNumber + half
 }
 
-// console.log(circleOfNumbers(10, 2));
+console.log(circleOfNumbers(10, 2));
